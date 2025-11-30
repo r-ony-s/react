4a810139b27c6de02c9dfc5442b66f0ec14df4eb
r-on-s <ronyarif700@gmail.com>
2025-11-30 14:23:08 +0600

    invoked function

--- pages/Head.jsx
+++ pages/Head.jsx
@@ -1,9 +1,18 @@
 const Head = () => {
-    const isLogin = true;
+    const isLogin = false;
     return (
         <div>
-            <h1>Log status</h1>  
-            {isLogin && <h1 style={{color: 'green'}}>Welcome back, user!</h1>}
+           {/* invoke function */}
+           <h1>Login Status</h1>
+           {(()=>{
+               if(isLogin){
+                return <h2>Welcome back, user!</h2>
+               }
+               else{
+                return <h2>Please log in to continue.</h2>
+               }
+           })()
+           }
         </div>
     );
 
