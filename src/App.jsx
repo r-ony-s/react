8afec0ebd7fa3d552da2effc898d69cbaa29c935
r-on-s <ronyarif700@gmail.com>
2025-12-04 21:28:18 +0600

    add and remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,48 @@
-import { useRef } from 'react';
-import {useState} from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const [list, setList] = useState([]);
-const [item, setItem] = useState('');
- const addtolist = () => {
-  list.push(item);
-  setList([...list]);
+  const [item, setItem] = useState('');
 
- }
- 
+  const addtolist = () => {
+    list.push(item);
+    setList([...list]);
+  };
+
+  const RemoveItem = (index) => {
+    alert("do you want to remove this item?");
+    list.splice(index, 1);
+    setList([...list]);
+  };
+
   return (
     <div>
-      <h1>{list.length}</h1>
-       <input onChange={(e)=>setItem(e.target.value)} placeholder='Item' />
-        <button onClick={addtolist}>Add</button>
+      <table>
+        <tbody>
+          {
+            list.length !== 0 &&
+            list.map((val, index) => {
+              return (
+                <tr key={index}>
+                  <td>{val}</td>
+                  <td>
+                    <button onClick={() => RemoveItem(index)}>
+                      Remove
+                    </button>
+                  </td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
 
+      <input
+        onChange={(e) => setItem(e.target.value)}
+        placeholder='Item'
+      />
+
+      <button onClick={addtolist}>Add</button>
     </div>
   );
 };
