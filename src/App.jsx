379749dbfff5dd5222677e1e545dd57e3be5405c
r-on-s <ronyarif700@gmail.com>
2025-12-04 21:55:35 +0600

    using usestate,create form without initialization

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import { useState } from 'react';
 
 const App = () => {
 
+  let [formObj,setObj]=useState(
+    {
+      firstName:"",
+      lastName:"",
+      city:"",  
+      gender:""
+    }
+  )
 
   return (
     <div>
        <form action="">
-        <input type="text" placeholder='First Name' />
-        <input type="text" placeholder='Last Name' />
-        <select >
+        <input value={formObj.firstName} placeholder='First Name' />
+        <input value={formObj.lastName} placeholder='Last Name' />
+        <select value={formObj.city} >
            <option value="">Choose city</option>
           <option value="Dhaka">Dhaka</option>
           <option value="Rangpur">Rangpur</option>
         </select>
-        <input type="radio" name='gender' />Male
-        <input type="radio" name='gender' />Female
+        <input checked={formObj.gender === "Male"} type="radio" name='gender' />Male
+        <input checked={formObj.gender === "Female"} type="radio" name='gender' />Female
         <button type='submit'>Submit</button>
        </form>
     </div>
